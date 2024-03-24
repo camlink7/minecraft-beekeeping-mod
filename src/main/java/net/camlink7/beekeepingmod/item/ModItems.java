6dcbda706a91b7d7bbfac99f93d1ff11a11b63a7
Camden <camdenlink7@gmail.com>
2024-03-24 17:58:28 -0400

    added sticky bun food item

--- src/main/java/net/camlink7/beekeepingmod/item/ModItems.java
+++ src/main/java/net/camlink7/beekeepingmod/item/ModItems.java
@@ -2,6 +2,7 @@ package net.camlink7.beekeepingmod.item;
 
 import net.camlink7.beekeepingmod.BeekeepingMod;
 import net.camlink7.beekeepingmod.item.custom.BeeSmokerItem;
+import net.minecraft.world.food.FoodProperties;
 import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.Item;
 import net.minecraftforge.eventbus.api.IEventBus;
@@ -19,6 +20,22 @@ public class ModItems {
                     .tab(ModCreativeModeTab.BEEKEEPING_TAB)
                     .stacksTo(1)
                     .setNoRepair()));
+
+    // Food
+    public static final RegistryObject<Item> STICKY_BUN = ITEMS.register("sticky_bun",
+            () -> new Item(new Item.Properties()
+                    .tab(ModCreativeModeTab.BEEKEEPING_TAB)
+                    .stacksTo(64)
+                    .food(Foods.STICKY_BUN)));
+
+    // Build Foods
+    public static class Foods {
+        public static final FoodProperties STICKY_BUN = new FoodProperties.Builder()
+                .nutrition(6)
+                .saturationMod(1f)
+                .alwaysEat()
+                .build();
+    }
 
     public static void register(IEventBus eventBus){
         ITEMS.register(eventBus);
